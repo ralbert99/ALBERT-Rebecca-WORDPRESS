2bf7fdea256ccecbe235b4c0147cdfcd1257e720
ralbert99 <33538199+ralbert99@users.noreply.github.com>
2018-03-10 13:39:49 -0600

    Check-In 8.2 Final

--- ALBERT-Rebecca-WORDPRESS/wordpress/wp-content/themes/cucumberescape/index.php
+++ ALBERT-Rebecca-WORDPRESS/wordpress/wp-content/themes/cucumberescape/index.php
@@ -4,9 +4,19 @@
 <link href="https://fonts.googleapis.com/css?family=Lato|PT+Sans+Narrow" rel="stylesheet">
 
 <?php
-get_header();
-get_template_part('main');
-get_template_part('columnlayout');
-get_footer();?>
+define( 'WP_USE_THEMES', false ); get_header();
+  get_template_part('main');
+  get_template_part('columnlayout');
+  if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+    the_title('<h2>' , '</h2>');
+    echo "<div class=''>";
+    echo "<p>" . the_content() . "</p>";
+    echo "<div>";
+
+  endwhile; else :
+  	esc_html_e( '<p>Sorry, no posts matched your criteria.</p>' );
+    endif;
+  get_footer();?>
 
 <!-- <?php echo "END OF " . basename(__FILE__); ?>-->
